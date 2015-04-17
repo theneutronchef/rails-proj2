d0c60c867d22c5a769f9c0162ecc49e6431d28e0
Japheth Wong <japhethjwong@gmail.com>
2015-04-17 12:36:03 -0700

    Converted tabs to spaces in models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ActiveRecord::Base
-	has_many :relations
-	validates :driver, presence: true
-	validates :number_of_seats, presence: true
-	validates :origin, presence: true
+  has_many :relations
+  validates :driver, presence: true
+  validates :number_of_seats, presence: true
+  validates :origin, presence: true
 end
