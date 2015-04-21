c92e7c4bef041ae02d9d1d2845c85520ef62b6a1
Gabriel Tan <tanjunxian@gmail.com>
2015-04-21 16:18:51 -0700

    Removed duplicate email_address field from Users, Updated db seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421224739) do
+ActiveRecord::Schema.define(version: 20150421230946) do
 
   create_table "cars", force: true do |t|
     t.integer  "driver"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20150421224739) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email_address"
     t.boolean  "is_driver"
     t.datetime "created_at"
     t.datetime "updated_at"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-gabriel = User.create(first_name: 'Gabriel', last_name: 'Tan', email_address: 'gabriel@groupool.com', is_driver: true)
-japheth = User.create(first_name: 'Japheth', last_name: 'Wong', email_address: 'japheth@groupool.com', is_driver: true)
-kelly = User.create(first_name: 'Kelly', last_name: 'Hong', email_address: 'kelly@groupool.com', is_driver: true)
+gabriel = User.create(first_name: 'Gabriel', last_name: 'Tan', email: 'gabriel@groupool.com', is_driver: true, encrypted_password: '#$taawktljasktlw4aaglj', password: 'password', password_confirmation: 'password')
+japheth = User.create(first_name: 'Japheth', last_name: 'Wong', email: 'japheth@groupool.com', is_driver: true, encrypted_password: '#$taawktljasktlw4aaglj', password: 'password', password_confirmation: 'password')
+kelly = User.create(first_name: 'Kelly', last_name: 'Hong', email: 'kelly@groupool.com', is_driver: true, encrypted_password: '#$taawktljasktlw4aaglj', password: 'password', password_confirmation: 'password')
 
 car1 = Car.create(driver: 1, number_of_seats: 5, origin: 'Berkeley')
 car2 = Car.create(driver: 2, number_of_seats: 5, origin: 'Emeryville')
 
-trip1= Trip.create(name: 'RubyOnRails Trip', hashid: 'invalidhash', date: '2015-04-15', time: '05:30:00.000000', destination: 'San Francisco')
+trip1= Trip.create(name: 'RubyOnRails Trip', hashid: 'invalidhash', datetime: '2015-04-15 05:30:00.000000', destination: 'San Francisco')
 
 Relation.create(user: gabriel, car: car1, trip: trip1)
 Relation.create(user: japheth, car: car2, trip: trip1)
